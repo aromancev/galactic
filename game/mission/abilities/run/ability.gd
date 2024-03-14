1c91c869cb24a0f33ad4b6b6be5095bd6cb7c595
Artem <41195617+a-romancev@users.noreply.github.com>
2024-03-14 20:53:16 +0500

    fix: format

--- game/mission/abilities/run/ability.gd
+++ game/mission/abilities/run/ability.gd
@@ -8,14 +8,12 @@ func use(target: Variant) -> void:
 	super(target)
 	_navigator.set_target(target as Vector3)
 
-
 func get_unit_velocity(delta: float) -> Vector3:
 	if !is_using() or !get_unit().is_on_floor():
 		return Vector3.ZERO
 
 	var unit: Unit = get_unit()
 	return _navigator.get_direction(delta) * unit.get_attribute_value("speed") * _RUNSPEEDMULT
-
 
 func _ready() -> void:
 	super()
